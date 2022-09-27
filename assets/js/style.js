13012bda3eb96a771fe99d6afe4bc66aa3412f0c
JacksonAhumada <jacksonahumada@gmail.com>
2022-09-26 20:52:13 -0600

    Please disregard this commit

--- assets/js/style.js
+++ assets/js/style.js
@@ -35,21 +35,41 @@ mealUnfold.addEventListener('click', (event) => {
 })
 
 //Cocktail
-drinkBtn.addEventListener('click', (event) => {
-    event.preventDefault();
-    var drinkString = drinkText.value
-    console.log(drinkString)
-
+function getCocktail() {
+    var drinkString = drinkText.value;
+    console.log(drinkString);
+  
     const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': '5b8d54141cmsh405f541d3bf16e0p10cdeajsn2acda58ff1e5',
-            'X-RapidAPI-Host': 'cocktail-by-api-ninjas.p.rapidapi.com'
-        }
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "5b8d54141cmsh405f541d3bf16e0p10cdeajsn2acda58ff1e5",
+        "X-RapidAPI-Host": "cocktail-by-api-ninjas.p.rapidapi.com",
+      },
     };
-    
-    fetch(`https://cocktail-by-api-ninjas.p.rapidapi.com/v1/cocktail?name=${drinkString}`, options)
-        .then(response => response.json())
-        .then(response => console.log(response))
-        .catch(err => console.error(err));
-});
+    fetch(
+      `https://cocktail-by-api-ninjas.p.rapidapi.com/v1/cocktail?name=${drinkString}`,
+      options
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        for (var i = 0; i < data.length; i++) {
+          var createDrinkRow = document.createElement("tr");
+          var drinkData = document.createElement("td");
+          var name = document.createElement("p");
+          name.textContent = data[i].name;
+  
+          drinkData.appendChild(name);
+          createDrinkRow.appendChild(drinkData);
+          drinkBody.appendChild(createDrinkRow);
+        }
+      })
+  
+      .catch((err) => console.error(err));
+  
+    console.log("drink");
+  }
+  
+  drinkBtn.addEventListener("click", getCocktail);
