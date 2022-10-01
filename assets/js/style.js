35e3474d3a33d9a391325b0875d38b9c8b846a99
JacksonAhumada <jacksonahumada@gmail.com>
2022-10-01 09:13:36 -0600

    bugs fixed. dont know what was different but this looks correct and functions properly

--- assets/js/style.js
+++ assets/js/style.js
@@ -38,7 +38,7 @@ function getMeal() {
     .then(function (data) {
       console.log(data);
       for (var i = 0; i < data.length; i++) {
-        var createRecipeRow = document.createElement("tr");
+        var createRecipeRow = document.createElement("li");
         var recipeData = document.createElement("td");
         var title = document.createElement("button");
 
